593e2d885d32da17c2e66f74f7c83913d13e6b5f
HankBedford <Zachary.Lynch1337@gmail.com>
2018-12-17 11:51:17 -0800

    Connected to API

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
 componentDidMount() {
   const key = '03c9de31969334077a097330d114675d'
 
-  axios.get()
+  axios.get(`https://www.food2fork.com/api/search?key=${key}&q=chicken%20breast&page=2`)
+    .then(res => {
+      console.log(res)
+    })
 }
 
   render() {
