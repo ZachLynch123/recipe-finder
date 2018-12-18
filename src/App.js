7faea16134a02b3c8ea54764e90929db22979621
Zach <Zachary.Lynch1337@gmail.com>
2018-12-18 07:22:18 -0800

    made request asyncronous, then got dummy data to develop with in order to not make too many api requests

--- src/App.js
+++ src/App.js
@@ -7,22 +7,33 @@ class App extends Component {
     super(props);
     
     this.state = {
-      
+      recipes: [],
+      url: 'https://www.food2fork.com/api/search?key=03c9de31969334077a097330d114675d'
     }
   }
 
+  async getRecipes() {
+    try{
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  // commenting this out to make less requests to api
+  /* componentDidMount() {
+    this.getRecipes();
+  } */
 
 
-componentDidMount() {
-  const key = '03c9de31969334077a097330d114675d'
 
-  axios.get(`https://www.food2fork.com/api/search?key=${key}&q=chicken%20breast&page=2`)
-    .then(res => {
-      console.log(res)
-    })
-}
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
       </div>
